5d3f793715cffa216ce7725c3668342362a19cda
Guilhermebtv <115501380+Guilhermebtv@users.noreply.github.com>
2024-09-02 16:29:06 -0300

    1

---
+++ SorteadordeNumero/SorteadordeNumero/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SorteadordeNumero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("SORTEADOR DE NÚMERO");
+            Console.WriteLine("------------------------");
+            Console.Write("Início: ");
+            int.TryParse(Console.ReadLine(), out a);
+
+            Console.Write("Fim: ");
+            int.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("-------------------------");
+
+            Console.SetCursorPosition(0, 5);
+            Random gerador = new Random();
+            int n = gerador.Next(a, b + 1);
+            Console.WriteLine("Sorteando ...");
+            Thread.Sleep(3000);
+            Console.SetCursorPosition(0, 5);
+            Console.Write($"Entre {a} e {b} sorteei o valor {n}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SorteadordeNumero/SorteadordeNumero/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("SorteadordeNumero")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SorteadordeNumero")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("21ec27a9-4424-4fee-930a-4e88e50be9ba")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
